bb25a0e6ef42d6b08af022c00f877b2c3713d09c
pulkitchauhan03 <pulkitchauhan03@gmail.com>
2023-02-09 06:17:42 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is Hello there how are ${count}
-          again and again and again
+          count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
