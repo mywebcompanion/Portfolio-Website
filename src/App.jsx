61c198a1a1e132a6cc461edfe395f785f3952091
pulkitchauhan03 <pulkitchauhan03@gmail.com>
2023-02-10 17:18:49 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import Socials from './components/Socials'
+import { useState } from "react";
+import "./App.css";
+import Socials from "./components/Socials";
+import Content from "./components/Content";
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-            Purple to blue
-        </span>
-      </button>
-      <Socials/>
-    </>
-  )
+    <div className="">
+      <Content />
+      <Socials />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
-  return <div>Navbar</div>
+  return (
+    <div className="h-16 flex justify-between items-center px-4">
+      <div>
+        <button>
+        <img src="#" alt="pulkitLogo" className="h-6 w-6"></img>
+        </button>
+      </div>
+      <div>
+        <button className="ml-4">Contact</button>
+        <button className="ml-4">Projects</button>
+        <button className="ml-4">Experience</button>
+        <button className="ml-4">Skills</button>
+        <button className="ml-4">Academics</button>
+        <button className="ml-4">Achievements</button>
+      </div>
+    </div>
+  );
 }
