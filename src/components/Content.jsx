e792d8e9cfa4cb8090d1289fa584fcda8532fb5a
pulkitchauhan03 <pulkitchauhan03@gmail.com>
2023-02-10 18:10:28 +0530

    added content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import Navbar from "./Navbar";
+import HackerImg from "../assets/hacker-image.png";
 
 export default function Content() {
   return (
     <div className="w-3/4 mx-auto ">
       <Navbar />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="flex">
+        <div className="w-1/2">First</div>
+        <div className="w-1/2 items-center">
+            <img src={HackerImg} alt="HackerImage" className="" />
+        </div>
+      </div>
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
         <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
           Purple to blue
         </span>
-      </button>
+      </button> */}
     </div>
   );
 }
